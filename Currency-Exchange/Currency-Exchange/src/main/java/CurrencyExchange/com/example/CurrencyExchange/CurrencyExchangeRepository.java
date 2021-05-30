7b04ea02982d9e27ec7d813b52cd82a1cf2867e3
Akash Rathi <akash92y@gmail.com>
2021-05-31 00:55:01 +0530

    Spring Boot Microservice Demo

---
+++ Currency-Exchange/Currency-Exchange/src/main/java/CurrencyExchange/com/example/CurrencyExchange/CurrencyExchangeRepository.java
@@ -0,0 +1,10 @@
+package CurrencyExchange.com.example.CurrencyExchange;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository 
+extends JpaRepository<CurrencyExchange, Long> {
+
+	CurrencyExchange findByFromAndTo(String from, String to);
+
+}
---
+++ Currency-Exchange/Currency-Exchange/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,enviornment)
+values(10005,'USD','INR',65,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,enviornment)
+values(10006,'EUR','INR',75,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,enviornment)
+values(10007,'AUD','INR',25,'');
